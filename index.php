a26d3e3cfa8fd521f999ae87bd02e7a384af98f4
Suraj Dubey <dubeysuraj864@gmail.com>
2022-05-30 13:29:09 +0530

    first

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="CSS/bootstrap.min.css"> -->
+</head>
+<body>
+
+       <?php 
+       echo "Stranger, Things", " \n The 5 Second rule <h1>studentstutorial.com</h1>";
+      
+       echo "<marquee > <b>fg</marquee> " ;
+
+    //    THE DIFFERENCE BTW PRINT AND ECHO IS ECHO CAN HAVE MULTIPLE STRINGS OR PRINT CANT
+
+
+
+    $a = "Suraj";
+    $b = "Dubey";
+    $c = 65;
+    $d = 54;
+
+    echo "$a <br> $b <br> $c";
+    print "<h1>$a</h1>";
+    print " $b<br>";
+    echo $c + $d;
+
+
+    //In php there are 9 superglobal variable available these are:
+    
+    // $GLOBALS
+
+    
+    // $_SERVER - is an superglobal variable containing information such as header,paths, and script locations.
+    
+    
+    // $_REQUEST - is used to collect form data.
+    
+    
+    
+    // $_POST - is used to collect form data. In POST you send the information to server in on way that is
+    
+    
+    
+    // $_GET - is used to collect form data. In get you send the information to server in two way: 
+        // 1.through URL
+        // 2. through form
+
+
+
+
+        
+    // $_FILES
+    // $_ENV
+    // $_COOKIE
+    // $_SESSION
+
+
+
+       ?>
+   
+<!-- <script src="JS/bootstrap.min.js"></script> -->
+</body>
+</html> 
